37bedbddcd6cb2face1c74770fc6e465098a2c05
Jules Schoonman <jules.schoonman@gmail.com>
2025-08-28 12:14:28 +0200

    Renamed src added export script

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import schemaExamples from "./src/examples";
+
+for (let i = 0; i < schemaExamples.length; i++) {
+  const example = schemaExamples[i];
+  const file = await Bun.file(`./json/schema-example-${i}.json`);
+  Bun.write(file, JSON.stringify(example, null, 4));
+}
